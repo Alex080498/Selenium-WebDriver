b9c14dfe4f5ae3f91b7ccf05c619fb90bf62f0b3
Alexandra <sasha.kanischewa@yandex.ru>
2021-11-21 15:02:53 +0300

    add click menu items test

--- Task3/src/main/java/Main.java
+++ Task3/src/main/java/Main.java
@@ -3,8 +3,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\tools\\chromeDriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver","C:\\tools\\chromeDriver\\chromedriver.exe");
+
 
         driver.get("http://localhost/litecart/admin/");
     }
---
+++ Task3/src/test/java/Litecart/TestClickMenuItems.java
@@ -0,0 +1,47 @@
+package Litecart;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class TestClickMenuItems {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromeDriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+    @Test
+    public void mainTest () {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("admin");
+        driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("admin");
+        driver.findElement(By.xpath("//button[@name=\"login\"]")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[@id=\"sidebar\"]")));
+        List<WebElement> elementList = driver.findElements(By.cssSelector("#box-apps-menu a"));
+        int numberOfMenuElements = elementList.size();
+        for (int i=0; i<numberOfMenuElements; i++) {
+            elementList = driver.findElements(By.cssSelector("#box-apps-menu a"));
+                    elementList.get(i).click();
+        }
+
+    }
+    @After
+    public void stop () {
+        driver.quit();
+        driver=null;
+    }
+}
