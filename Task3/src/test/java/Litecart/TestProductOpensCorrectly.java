04564209eee6f5a4192a90952e19fedc7762729e
Alexandra <sasha.kanischewa@yandex.ru>
2021-12-01 23:17:12 +0300

    modified task

--- Task3/src/test/java/Litecart/TestProductOpensCorrectly.java
+++ Task3/src/test/java/Litecart/TestProductOpensCorrectly.java
@@ -31,17 +31,33 @@ public class TestProductOpensCorrectly {
         String priceInCatalog = driver.findElements(By.xpath("//*[contains(@id, 'box-campaigns')]//li")).get(0).findElement(By.className("regular-price")).getText(); //исходная цена
         String salePriceInCatalog = driver.findElements(By.xpath("//*[contains(@id, 'box-campaigns')]//li")).get(0).findElement(By.className("campaign-price")).getText();// цена со скидкой
         String crossedPriceInCatalog = driver.findElements(By.xpath("//*[contains(@id, 'box-campaigns')]//li")).get(0).findElement(By.className("regular-price")).getCssValue("text-decoration-line");// цена перечеркнута
+
         String greyPriceInCatalog = driver.findElements(By.xpath("//*[contains(@id, 'box-campaigns')]//li")).get(0).findElement(By.className("regular-price")).getCssValue("color");//цена серая
+        String[] greys = greyPriceInCatalog.replace("rgba(", "").replace(")", "").split(",");
+        int r = Integer.parseInt(greys[0].trim());
+        int g = Integer.parseInt(greys[1].trim());
+        int b = Integer.parseInt(greys[2].trim());
+        Assert.assertEquals(r,g);
+        Assert.assertEquals(g,b);
+
         String boldSalePriceInCatalog = driver.findElements(By.xpath("//*[contains(@id, 'box-campaigns')]//li")).get(0).findElement(By.className("campaign-price")).getAttribute("tagName");//цена полужирная
+
         String redSalePriceInCatalog = driver.findElements(By.xpath("//*[contains(@id, 'box-campaigns')]//li")).get(0).findElement(By.className("campaign-price")).getCssValue("color");//цена красная
+        String[] reds = redSalePriceInCatalog.replace("rgba(", "").replace(")", "").split(",");
+        int r1 = Integer.parseInt(reds[0].trim());
+        int g1 = Integer.parseInt(reds[1].trim());
+        int b1 = Integer.parseInt(reds[2].trim());
+        Assert.assertEquals(g1,0);
+        Assert.assertEquals(b1,0);
+
         String fontPriceInCatalog = driver.findElements(By.xpath("//*[contains(@id, 'box-campaigns')]//li")).get(0).findElement(By.className("regular-price")).getCssValue("font-size").substring(0,2);//размер шрифта обычной цены
         String fontSalePriceInCatalog = driver.findElements(By.xpath("//*[contains(@id, 'box-campaigns')]//li")).get(0).findElement(By.className("campaign-price")).getCssValue("font-size").substring(0,2);//размер шрифта цены со скидкой
         int sizeOfRegularPrice = Integer.parseInt(fontPriceInCatalog);
         int sizeOfSalePrice = Integer.parseInt(fontSalePriceInCatalog);
 
         Assert.assertTrue(crossedPriceInCatalog.equals("line-through"));
-        Assert.assertTrue(greyPriceInCatalog.equals("rgba(119, 119, 119, 1)"));
-        Assert.assertTrue(redSalePriceInCatalog.equals("rgba(204, 0, 0, 1)"));
+        //Assert.assertTrue(greyPriceInCatalog.equals("rgba(119, 119, 119, 1)"));
+        //Assert.assertTrue(redSalePriceInCatalog.equals("rgba(204, 0, 0, 1)"));
         Assert.assertTrue(boldSalePriceInCatalog.equals("STRONG"));
         Assert.assertTrue(sizeOfSalePrice > sizeOfRegularPrice);
 
@@ -49,9 +65,25 @@ public class TestProductOpensCorrectly {
         Assert.assertTrue(driver.findElement(By.tagName("h1")).getText().equals(nameInCatalog)); // сравнение названий
         Assert.assertTrue(driver.findElement(By.className("regular-price")).getText().equals(priceInCatalog)); // сравнение обычных цен
         Assert.assertTrue(driver.findElement(By.className("campaign-price")).getText().equals(salePriceInCatalog));// сравнение акционных цен
-        Assert.assertTrue(driver.findElement(By.className("regular-price")).getCssValue("color").equals("rgba(102, 102, 102, 1)")); // обычная цена на детальной странице серая
+
+        String greyPriceInDetailPage = driver.findElement(By.className("regular-price")).getCssValue("color"); // обычная цена на детальной странице серая
+        String[] greysDetailPage = greyPriceInDetailPage.replace("rgba(", "").replace(")", "").split(",");
+        int r3 = Integer.parseInt(greysDetailPage[0].trim());
+        int g3 = Integer.parseInt(greysDetailPage[1].trim());
+        int b3 = Integer.parseInt(greysDetailPage[2].trim());
+        Assert.assertEquals(r3,g3);
+        Assert.assertEquals(g3,b3);
+
         Assert.assertTrue(driver.findElement(By.className("regular-price")).getCssValue("text-decoration-line").equals("line-through")); // обычная цена перечеркнута
-        Assert.assertTrue(driver.findElement(By.className("campaign-price")).getCssValue("color").equals("rgba(204, 0, 0, 1)")); // акционная цена красная
+
+        String redPriceOnDetailPage = driver.findElement(By.className("campaign-price")).getCssValue("color"); // акционная цена красная
+        String[] redsDetailPage = redPriceOnDetailPage.replace("rgba(", "").replace(")", "").split(",");
+        int r4 = Integer.parseInt(redsDetailPage[0].trim());
+        int g4 = Integer.parseInt(redsDetailPage[1].trim());
+        int b4 = Integer.parseInt(redsDetailPage[2].trim());
+        Assert.assertEquals(g4,0);
+        Assert.assertEquals(b4,0);
+
         Assert.assertTrue(driver.findElement(By.className("campaign-price")).getAttribute("tagName").equals("STRONG")); // акционная цена полужирная
          String  fontOfRegPrice = driver.findElement(By.className("regular-price")).getCssValue("font-size").substring(0,2);
          String fontOfSaPrice = driver.findElement(By.className("campaign-price")).getCssValue("font-size").substring(0,2);
